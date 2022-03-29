24ce989730e2c89219d427cffe78982007cf6d6f
DD-Hieu <doduchieu13101999@gmail.com>
2022-03-29 10:27:18 +0700

    add about page link

--- components/NavbarMenu.js
+++ components/NavbarMenu.js
@@ -1,12 +1,20 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Link from 'next/link';
 
 const NavbarMenu = () => (
     <Navbar bg='dark' variant='dark'>
         <Navbar.Brand>My next app</Navbar.Brand>
         <Nav>
-            <Nav.Link>Posts</Nav.Link>
-            <Nav.Link>About</Nav.Link>
+            <Link href='/home' passHref>
+                <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link href='/posts' passHref>
+                <Nav.Link>Posts</Nav.Link>
+            </Link>
+            <Link href='/about'passHref>
+                <Nav.Link>About</Nav.Link>
+            </Link>
         </Nav>
     </Navbar>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Layout from "../components/Layout"
+import Link from 'next/link'
+
 export default function Home() {
   return  (
     <Layout>
       <h1>My next app</h1>
+      <Link href = '/about'>About page</Link>
     </Layout>
   )
 }
